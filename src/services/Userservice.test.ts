2b79d7257541b2e85227b53e69307b893118110a
lucarl07 <junitinho2007@gmail.com>
2024-09-27 12:48:50 -0300

    ✅ tests: 2/6 tests done

--- src/services/Userservice.test.ts
+++ src/services/Userservice.test.ts
@@ -19,9 +19,24 @@ describe('Testando User service', () => {
     //se não existir, ele cria, se existir o "force", faz com que ele delete, e cria uma nova
     beforeAll(async () => {
         await User.sync({ force: true })
+        expect(2 + 2).toBe(4)
     })
 
-    it("Criar um usuário corretamente", () => {
-        expect(2 + 2).toBe(4)
+    it("Deve criar um usuário corretamente", async () => {
+        const newUser = await UserService.createUser(email, password) as UserInstance
+        expect(newUser).not.toBeInstanceOf(Error)
+        expect(newUser).toHaveProperty('id')
+        expect(newUser.email).toBe(email)
+        expect(newUser.password).not.toBe(password)
+    })
+
+    it("Não deve conseguir criar um usuário", async () => {
+        // Primeiro usuário:
+        await UserService.createUser(email, 'SouOPrimeiro')
+
+        // Segundo usuário, com o mesmo e-mail do primeiro:
+        const secondUser = await UserService.createUser(email, 's0v0s3gvnd0')
+
+        expect(secondUser).toBeInstanceOf(Error)
     })
 })
