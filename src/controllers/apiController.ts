64d84c83c47d936015aadc16e7406cb4faf5a676
lucarl07 <junitinho2007@gmail.com>
2024-09-30 11:23:12 -0300

    ✨ feat: Add remaining HTTP responses

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import * as UserService from '../services/Userservice'
-import { User } from '../models/User';
 
 export const ping = (req: Request, res: Response) => {
     res.json({ pong: true });
@@ -13,13 +12,14 @@ export const register = async (req: Request, res: Response) => {
         const newUser = await UserService.createUser(email, password)
 
         if (newUser instanceof Error) {
+            res.status(409)
             return res.json({ error: newUser.message })
         } else {
             res.status(201)
             return res.json({ id: newUser.id })
         }
     }
-    res.json({ error: 'E-mail e/ou senha não enviados.' });
+    res.status(400).json({ error: 'E-mail e/ou senha não enviados.' });
 }
 
 export const login = async (req: Request, res: Response) => {
@@ -30,21 +30,31 @@ export const login = async (req: Request, res: Response) => {
         const user = await UserService.findByEmail(email)
 
         if (user && await UserService.matchPassword(password, user.password)) {
-            res.json({ status: true })
-            return
+            res.status(201)
+            return res.json({ status: true })
+        } else {
+            res.status(401)
+            return res.json({ status: false })
         }
     }
 
-    res.json({ status: false });
+    res.status(400).json({ error: 'E-mail e/ou senha não enviados.' });
 }
 
 export const list = async (req: Request, res: Response) => {
-    let users = await UserService.all()
-    let list: string[] = [];
+    try {
+        let users = await UserService.all()
+        let list: string[] = [];
 
-    for (let i in users) {
-        list.push(users[i].email);
+        for (let i in users) {
+            list.push(users[i].email);
+        }
+
+        return res.status(200).json({ list });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ 
+            error: 'Erro interno no servidor.'
+        });
     }
-
-    res.json({ list });
 }
