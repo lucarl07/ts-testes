7e6f7da97e120c54f93ec61eff78c381bae54152
lucarl07 <junitinho2007@gmail.com>
2024-10-07 11:53:13 -0300

    🚧 Set up what tests should be done

--- src/routes/api.test.ts
+++ src/routes/api.test.ts
@@ -7,9 +7,11 @@ beforeAll(async () => {
 })
 
 describe("Ao acessar POST /register", () => {    
-    
+    it.todo("Não deve registrar um usuário sem a senha.")
+    it.todo("Não deve registrar um usuário sem o e-mail.")
+    it.todo("Não deve registrar um usuário sem os dados.")
 })
 
 describe("Ao acessar POST /login", () => { // No momento, deve ser sempre executado após os testes de /POST register
-    
+    it.todo("Deve logar corretamente.")
 })
