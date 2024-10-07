693d12a4d6e0e26bb376257ec9137b92ee70242a
lucarl07 <junitinho2007@gmail.com>
2024-10-07 12:08:42 -0300

    ✅ test: When accessing POST /register...
    - An user shouldn't be registered without a password
    - An user shouldn't be registered without an e-mail
    - An user shouldn't be registered without any data

--- src/routes/api.test.ts
+++ src/routes/api.test.ts
@@ -3,15 +3,50 @@ import app from '../app'
 import { User } from '../models/User'
 
 beforeAll(async () => {
-    await User.sync({ force: true })
+  await User.sync({ force: true })
 })
 
-describe("Ao acessar POST /register", () => {    
-    it.todo("Não deve registrar um usuário sem a senha.")
-    it.todo("Não deve registrar um usuário sem o e-mail.")
-    it.todo("Não deve registrar um usuário sem os dados.")
+describe("Ao acessar POST /register", () => {
+  it("Não deve registrar um usuário sem a senha", async () => {
+    const password = '320jrf490j4'
+
+    await request(app)
+      .post('/register')
+      .send(`password=${password}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .then((res) => {
+        expect(res.body).toHaveProperty('error')
+      })
+  })
+
+  it("Não deve registrar um usuário sem o e-mail", async () => {
+    const email = 'umBomEmail@email.com'
+    
+    await request(app)
+      .post('/register')
+      .send(`email=${email}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .then((res) => {
+        expect(res.body).toHaveProperty('error')
+      })
+  })
+  
+  it("Não deve registrar um usuário sem os dados", async () => {
+    await request(app)
+      .post('/register')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .then((res) => {
+        expect(res.body).toHaveProperty('error')
+      })
+  })
 })
 
 describe("Ao acessar POST /login", () => { // No momento, deve ser sempre executado após os testes de /POST register
-    it.todo("Deve logar corretamente.")
+  it.todo("Deve logar corretamente.")
 })
