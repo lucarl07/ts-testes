009fb04f02f9f85dde5b78a0567067681070af34
lucarl07 <junitinho2007@gmail.com>
2024-10-07 12:13:01 -0300

    🧪 test: Correctly create an account with POST /register

--- src/routes/api.test.ts
+++ src/routes/api.test.ts
@@ -45,8 +45,24 @@ describe("Ao acessar POST /register", () => {
         expect(res.body).toHaveProperty('error')
       })
   })
+
+  it("Deve criar uma conta corretamente", async () => {
+    const email = 'umBomEmail@email.com'
+    const password = '320jrf490j4'
+    
+    await request(app)
+      .post('/register')
+      .send(`email=${email}&password=${password}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .then((res) => {
+        expect(res.body).toHaveProperty('id')
+        expect(res.body.id).toBeTruthy()
+      })
+  })
 })
 
 describe("Ao acessar POST /login", () => { // No momento, deve ser sempre executado após os testes de /POST register
-  it.todo("Deve logar corretamente.")
+  it.todo("Deve logar corretamente")
 })
